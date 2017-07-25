b1d0057e2b0256acacf872db6741856e3b04fb3c
Reyn-L <Reyn.Leong@gmail.com>
2017-07-25 10:30:10 -1000

    first table

--- normal_cars.sql
+++ normal_cars.sql
@@ -1,3 +1,16 @@
 \c reyn
 
+DROP DATABASE IF EXISTS normal_cars;
+DROP USER IF EXISTS normal_user;
+
 CREATE USER normal_user;
+
+CREATE DATABASE normal_cars WITH OWNER normal_user;
+
+\c normal_cars;
+
+CREATE TABLE model_cars (
+id SERIAL NOT NULL PRIMARY KEY,
+model_code VARCHAR(100),
+model_title VARCHAR(100)
+);
