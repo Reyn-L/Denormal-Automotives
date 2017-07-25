19bc828f7300ba087b57b8a57e809eee8374f146
Reyn-L <Reyn.Leong@gmail.com>
2017-07-25 09:54:55 -1000

    denorm 2nd query

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -1,15 +1,19 @@
-\c reyn
+-- \c reyn
 
-DROP DATABASE IF EXISTS denormal_cars;
-DROP USER IF EXISTS denormal_user;
+-- DROP DATABASE IF EXISTS denormal_cars;
+-- DROP USER IF EXISTS denormal_user;
 
-CREATE USER denormal_user;
+-- CREATE USER denormal_user;
 
-CREATE DATABASE denormal_cars WITH OWNER denormal_user;
+-- CREATE DATABASE denormal_cars WITH OWNER denormal_user;
 
-\c denormal_cars
-\i scripts/denormal_data.sql
+-- \c denormal_cars
+-- \i scripts/denormal_data.sql
 
 
-SELECT DISTINCT make_title
-FROM car_models;
+-- SELECT DISTINCT make_title
+-- FROM car_models;
+
+SELECT DISTINCT model_title
+FROM car_models
+WHERE make_code = 'VOLKS';
