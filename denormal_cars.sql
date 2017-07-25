78227688fd576f2f4d01e28df61e33b1bce9579a
Reyn-L <Reyn.Leong@gmail.com>
2017-07-25 09:48:36 -1000

    denorm step 5 query

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -1,11 +1,15 @@
 \c reyn
 
+DROP DATABASE IF EXISTS denormal_cars;
 DROP USER IF EXISTS denormal_user;
 
 CREATE USER denormal_user;
 
-DROP DATABASE IF EXISTS denormal_cars;
 CREATE DATABASE denormal_cars WITH OWNER denormal_user;
 
 \c denormal_cars
 \i scripts/denormal_data.sql
+
+
+SELECT DISTINCT make_title
+FROM car_models;
