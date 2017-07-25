c8b423d0a4642fbda272f50c02c58aae718c8a3b
Reyn-L <Reyn.Leong@gmail.com>
2017-07-25 10:30:46 -1000

    3rd table

--- normal_cars.sql
+++ normal_cars.sql
@@ -20,3 +20,10 @@ id SERIAL NOT NULL PRIMARY KEY,
 make_code VARCHAR(100),
 make_title VARCHAR(100)
 );
+
+CREATE TABLE cars (
+id SERIAL NOT NULL PRIMARY KEY,
+year INT,
+model_cars_id INT REFERENCES model_cars(id),
+make_cars_id INT REFERENCES make_cars(id)
+);
